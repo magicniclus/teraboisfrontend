006460be577d07f39381738766eb4f878339a001
Nicolas <casteranicolas.contact@gmail.com>
2022-06-24 15:50:01 +0200

    Update Comments.js
    Add title in the page

--- src/component/comments/Comments.js
+++ src/component/comments/Comments.js
@@ -2,9 +2,12 @@ import React from 'react';
 import "./_comments.scss"
 
 const Comments = () => {
+    
+
     return (
         <div className='commentsContainer'>
-            
+            <h1>Ils nous on fait confiance</h1>
+
         </div>
     );
 };
