3898490bbb5f43959165338cbbc118436b3d1d13
Nicolas <casteranicolas.contact@gmail.com>
2022-06-24 14:20:11 +0200

    Create Comments.js

---
+++ src/component/comments/Comments.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./_comments.scss"
+
+const Comments = () => {
+    return (
+        <div className='commentsContainer'>
+            
+        </div>
+    );
+};
+
+export default Comments;
