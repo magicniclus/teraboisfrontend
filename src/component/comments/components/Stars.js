936acf5884e63555a6406f31e32c263be9637bf7
Nicolas <casteranicolas.contact@gmail.com>
2022-07-05 15:55:03 +0200

    Create Stars.js

---
+++ src/component/comments/components/Stars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Stars = (props) => {
+    const stars = props.stars;
+
+    return (
+        <div className='starsContainer'>
+            
+        </div>
+    );
+};
+
+export default Stars;
