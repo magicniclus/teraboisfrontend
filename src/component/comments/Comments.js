1f0b9bf0f8e945b9a402ba79036507430029b48c
Nicolas <casteranicolas.contact@gmail.com>
2022-07-05 15:54:42 +0200

    Update Comments.js

--- src/component/comments/Comments.js
+++ src/component/comments/Comments.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { showMeComments } from '../../redux/actions/action';
+import Cards from './components/Cards';
 import "./_comments.scss"
 
 const Comments = () => {
-    
+    const [isLoading, setIsLoading] = useState(false)
+
+    const dispatch = useDispatch();
+
+    const state = useSelector(state => state.comments)
+
+    useEffect(()=>{
+        addComments()
+    }, [])
+
+    const addComments = async ()=>{
+        await setIsLoading(true)
+        await dispatch(showMeComments())
+        await setIsLoading(false)
+    }
+
+    const loader = ()=>{
+        return(
+            <h4>Loader</h4>
+        )
+    }
+
+    const getComment = ()=>{
+        if(state !== []){
+            return(
+                <>
+                    <h4>Not loader</h4>
+                    {
+                        state.map((comm)=>
+                        <div key={comm.id} className='cardsContainer'>
+                            <Cards name={comm.attributes.name} commentaire={comm.attributes.commentaire} stars={comm.attributes.note}/>
+                        </div>
+                        )
+                    }
+                </>
+            )
+        }
+    }
 
     return (
         <div className='commentsContainer'>
-            <h1>Ils nous on fait confiance</h1>
-
+            <h3>Ils nous on fait confiance</h3>
+            <div className="commentContainer">
+                {
+                    isLoading ? loader():getComment()
+                }
+            </div>
         </div>
     );
 };
