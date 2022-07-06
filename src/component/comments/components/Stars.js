2fe363a4bc2ee75b99528b5874297dffeb2135c4
Nicolas <casteranicolas.contact@gmail.com>
2022-07-06 11:49:27 +0200

    Update Stars.js

--- src/component/comments/components/Stars.js
+++ src/component/comments/components/Stars.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import "./_stars.scss"
 
 const Stars = (props) => {
     const stars = props.stars;
+    const starInit = [1, 2, 3, 4, 5];
+
+    const starCheck=(star) =>{
+        let index = starInit.indexOf(star);
+        if (index + 1 <= stars) return <i key={star} className="fas fa-star check"></i>
+        else return <i key={star} className="fas fa-star uncheck"></i>
+    }
 
     return (
-        <div className='starsContainer'>
-            
-        </div>
+        <>
+            {
+                starInit.map((star) => { return starCheck(star) })
+            }
+        </>
     );
 };
 
