bff82b877682032596a4e721ce87d099aa4aeacc
Nicolas <casteranicolas.contact@gmail.com>
2022-07-22 11:51:35 +0200

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,11 +3,11 @@ import HomeNavigation from '../component/HomeNavigation';
 import { NavLink } from 'react-router-dom';
 import Button from "../component/Button"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 import DropDown from '../component/DropDown';
 import TitleAndText from '../component/TitleAndText';
 import ImageAndText from '../component/ImageAndText';
-import {gsap} from 'gsap';
+import { gsap } from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Comments from '../component/comments/Comments';
 
@@ -30,7 +30,7 @@ const HomePage = () => {
     /* It's a hook that allows you to perform side effects in function components. */
     useEffect(() => {
         document.title = "TeraBois | Accueil"
-        window.scrollTo(0, 0); 
+        window.scrollTo(0, 0);
         gsap.to(navBar.current, {
             duration: 0.3,
             y: 0,
@@ -43,16 +43,17 @@ const HomePage = () => {
                 start: "bottom top+=76"
             }
         })
-        gsap.fromTo(".titleAndText", {x: 3000}, {duration:1, x: 0, clearProps: "x", ease: "back.out(0.3)"})
-        gsap.fromTo(".specialiste", {opacity: 0}, { duration: 1, delay:0.7, opacity: 1, clearProps:'x', ease: "back.out"})
-        gsap.fromTo(".homePageHeader", {opacity: 0}, {delay:0.5, duration:1, opacity: 1, ease: "back.out(0.3)"})
-        gsap.fromTo(".bottomContainerTitle", {x: 3000}, {duration:1, x: 0, clearProps: "x", ease: "back.out(0.3)"})
-        gsap.fromTo(".avantageContainer", {x: 3000}, {duration:1, x: 0, clearProps: "x", ease: "back.out(0.3)"})
+        gsap.fromTo(".titleAndText", { x: 2000 }, { duration: 1, x: 0, clearProps: "x", ease: "back.out(0.3)" })
+        gsap.fromTo(".titleAndText", { opacity: 0 }, { duration: 1, opacity: 1, ease: "back.out(0.3)" })
+        gsap.fromTo(".specialiste", { opacity: 0 }, { duration: 1, delay: 0.5, opacity: 1, clearProps: 'x', ease: "back.out" })
+        gsap.fromTo(".homePageHeader", { opacity: 0 }, { delay: 0.5, duration: 0.7, opacity: 1, ease: "back.out(0.3)" })
+        gsap.fromTo(".bottomContainerTitle", { x: 3000 }, { duration: 1, x: 0, clearProps: "x", ease: "back.out(0.3)" })
+        gsap.fromTo(".avantageContainer", { x: 3000 }, { duration: 1, x: 0, clearProps: "x", ease: "back.out(0.3)" })
     }, [])
 
 
 
-   /* It's a function that returns a JSX element */
+    /* It's a function that returns a JSX element */
     return (
         <>
             <header className="homePageHeader">
@@ -63,66 +64,66 @@ const HomePage = () => {
                     <HomeNavigation visibility={false} gsap={true} />
                 </div>
                 <div className="containerTop">
-                    <p className="littleConnect">Déjà un projet avec nous ? <br/> <NavLink to='/connection'>Connectez-vous</NavLink></p>
+                    <p className="littleConnect">Déjà un projet avec nous ? <br /> <NavLink to='/connection'>Connectez-vous</NavLink></p>
                     <TitleAndText title="Maison TeraBois" text='“Notre savoir-faire au service de votre bien-être”' img="" />
-                        <Button value="Isoler mon logement" link="/nous-contacter"/>
+                    <Button value="Isoler mon logement" link="/nous-contacter" />
                     <section className="specialiste">
-                        <ImageAndText addClass="imageAndText" sens="left" img="./img/test/isolation/isolationQuatre.jpeg" alt="isolation interieur" title="Spécialiste de l’économie d’énergie " text= "TeraBois, spécialiste en économie d'énergie de l'habitat depuis plus de 10 ans, TeraBois est avant tout une entreprise familiale et artisanale." textTwo="Notre savoir faire nous a permis de nous piositionner comme l'entreprise leader dans son domaine." /> 
+                        <ImageAndText addClass="imageAndText" sens="left" img="./img/test/isolation/isolationQuatre.jpeg" alt="isolation interieur" title="Spécialiste de l’économie d’énergie " text="TeraBois, spécialiste en économie d'énergie de l'habitat depuis plus de 10 ans, TeraBois est avant tout une entreprise familiale et artisanale." textTwo="Notre savoir faire nous a permis de nous piositionner comme l'entreprise leader dans son domaine." />
                     </section>
                     <NavLink to="/about" className="btnUn">En savoir plus</NavLink>
-                </div>    
+                </div>
                 <section className="smallBottomContainer">
-                    <DropDown question="Pourquoi bien isoler son logement ? " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis."/>
-                    <DropDown question="Les aides de l'état " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis."/>
-                    <DropDown question="Comment ça fonctionne ? " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis."/>
-                    <DropDown question="Qui sommes-nous ? " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis."/>
+                    <DropDown question="Pourquoi bien isoler son logement ? " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis." />
+                    <DropDown question="Les aides de l'état " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis." />
+                    <DropDown question="Comment ça fonctionne ? " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis." />
+                    <DropDown question="Qui sommes-nous ? " text="Lorem ipsum dolor sit amet. Aut nesciunt distinctio ab quasi ipsam non voluptatem inventore aut explicabo modi eum porro beatae. Est commodi officiis et fugiat nemo sit minus voluptatum. Sed porro error aut inventore minima vel architecto velit ea accusantium officiis." />
                     <div className="btnContainer">
-                        <Button value="Isoler mon logement" link="/nous-contacter"/>
+                        <Button value="Isoler mon logement" link="/nous-contacter" />
                     </div>
                 </section>
                 <section className="bottomContainer">
                     <h2 className='bottomContainerTitle'>Pourquoi bien faire isoler son logement ?</h2>
                     <div className="avantageContainer">
                         <div className="aidesContainer">
-                            <h3 className='aides'>Jusqu'à <br/> 50 000€ d'aîde</h3>
+                            <h3 className='aides'>Jusqu'à <br /> 50 000€ d'aîde</h3>
                         </div>
                         <div className="checkUn check">
-                            <FontAwesomeIcon icon={faCircleCheck} /> 
+                            <FontAwesomeIcon icon={faCircleCheck} />
                             <span>Facture réduite</span>
                         </div>
                         <div className="checkDeux check">
-                            <FontAwesomeIcon icon={faCircleCheck} /> 
+                            <FontAwesomeIcon icon={faCircleCheck} />
                             <span>Gain en confort</span>
                         </div>
                         <div className="checkTrois check">
-                            <FontAwesomeIcon icon={faCircleCheck} /> 
+                            <FontAwesomeIcon icon={faCircleCheck} />
                             <span>Maison plus saine</span>
                         </div>
                         <div className="checkQuatre check">
-                            <FontAwesomeIcon icon={faCircleCheck} /> 
+                            <FontAwesomeIcon icon={faCircleCheck} />
                             <span>Participation à l'éffort écologique</span>
                         </div>
                         <div className="checkQuatre check">
-                            <FontAwesomeIcon icon={faCircleCheck} /> 
+                            <FontAwesomeIcon icon={faCircleCheck} />
                             <span>Strict respect des normes de l'État</span>
                         </div>
-                            <div className="endContainer"><NavLink to="/nous-contacter" className="btnUn">Isoler mon <br/>logement</NavLink></div>
+                        <div className="endContainer"><NavLink to="/nous-contacter" className="btnUn">Isoler mon <br />logement</NavLink></div>
                     </div>
                     <div className="garantiesContainer">
                         <div className="garanties">
-                            <img src="./img/SVG/1x/shield.png" alt="shield"/>
+                            <img src="./img/SVG/1x/shield.png" alt="shield" />
                             <p>Garantie décénale</p>
                         </div>
                         <div className="garanties">
-                            <img src="./img/SVG/1x/hand.png" alt="hand"/>
-                            <p>Devis gratuit <br/> en 24/48h</p>
+                            <img src="./img/SVG/1x/hand.png" alt="hand" />
+                            <p>Devis gratuit <br /> en 24/48h</p>
                         </div>
                         <div className="garanties">
-                            <img src="./img/SVG/1x/feather.png" alt="hand"/>
-                            <p>Données 100% <br/> sécurisées</p>
+                            <img src="./img/SVG/1x/feather.png" alt="hand" />
+                            <p>Données 100% <br /> sécurisées</p>
                         </div>
                         <div className="garanties">
-                            <img src="./img/SVG/1x/phone.png" alt="hand"/>
+                            <img src="./img/SVG/1x/phone.png" alt="hand" />
                             <p>SAV 7/7j</p>
                         </div>
                     </div>
