cf4b5bbcd5f8ed8701d3e4d0e8e261c6bd16733c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-03-06 11:21:45 +0100

    Update TitleAndText.js

--- src/component/TitleAndText.js
+++ src/component/TitleAndText.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 /**
  * It takes in a title and text as props, and returns a title and text
@@ -6,40 +6,37 @@ import React from 'react';
  * @returns A React component that displays a title and text.
  */
 const TitleAndText = (props) => {
+  /* Assigning the value of the title prop to the title variable. */
+  const title = props.title;
 
-    /* Assigning the value of the title prop to the title variable. */
-    const title = props.title;
+  const susTitle = props.susTitle;
 
-    /* Assigning the value of the text prop to the text variable. */
-    const text = props.text;
+  /* Assigning the value of the text prop to the text variable. */
+  const text = props.text;
 
-    const prestation = props.prestation;
+  const prestation = props.prestation;
 
-
-    /* Returning a React component that displays a title and text. */
-    return (
-        <div className='titleAndText'>
-            <h1>{title}</h1>
-            <p className="description">
-                {text}
-            </p>
-            <div className='allPrestation'>
-                {       
-                        typeof prestation === 'object' ? prestation.map((el, idx)=>{
-                        if(idx === prestation.length-1){
-                            return (
-                                <span key={el}>{el}</span>
-                            )
-                        }else{
-                            return(
-                                <span key={el}>{el} |&nbsp;</span>
-                            )
-                        }
-                    }): null
-                }
-            </div>
-        </div>
-    );
+  /* Returning a React component that displays a title and text. */
+  return (
+    <div className="titleAndText">
+      <h1>{title}</h1>
+      <p className="description">{text}</p>
+      <div className="allPrestation">
+        {typeof prestation === "object"
+          ? prestation.map((el, idx) => {
+              if (idx === prestation.length - 1) {
+                return <span key={el}>{el}</span>;
+              } else {
+                return <span key={el}>{el} |&nbsp;</span>;
+              }
+            })
+          : null}
+      </div>
+      {susTitle ? <h3 className="qualibat">{susTitle}</h3> : null}
+    </div>
+  );
 };
+
+//Entreprise Qualibat RGE
 
 export default TitleAndText;
